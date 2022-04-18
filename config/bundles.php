c90f711ffefdbaf5c777617ea783e6f30ef4c27d
odyssee974 <manu.melchior@gmail.com>
2022-04-18 23:09:19 +0200

    add procfile

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
